7b2dfb57cc4ebb734ef83e5e602ec525e9908086
pranavpdx <pranavpdx@gmail.com>
2019-02-04 21:51:09 -0800

    Update Life.java

--- src/Life.java
+++ src/Life.java
@@ -1,3 +1,8 @@
+/*
+ * This is a simulation of the game Conway's Life
+ * Author: Pranav Sharma
+ * Date: 9/25/18
+ */
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
@@ -10,9 +15,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Life implements MouseListener, ActionListener, Runnable {
-	boolean[][] cells = new boolean[17][17];
+	// creates an array representing all the cells on the JPanel
+	boolean[][] cells = new boolean[10][10];
+	// creates frame that all operations are done in
 	JFrame frame = new JFrame("Conway Life's Simulation");
+	// creates the panel of cells
 	LifePanel panel = new LifePanel(cells);
+	// creates the container that holds all the buttons in the program
 	Container south = new Container();
 	JButton step = new JButton("Step");
 	JButton start = new JButton("Start");
@@ -22,12 +31,17 @@ public class Life implements MouseListener, ActionListener, Runnable {
 	boolean running = false;
 	int speed = 1;
 
+	// constructor that creates the actual game
 	public Life() {
+		// sets properties of the frame
 		frame.setSize(600, 600);
 		frame.setLayout(new BorderLayout());
+
+		// adds panel to frame and sets layout
 		frame.add(panel, BorderLayout.CENTER);
 		panel.addMouseListener(this);
 
+		// sets layout and adds all buttons to the container named south
 		south.setLayout(new GridLayout(1, 5));
 		south.add(step);
 		step.addActionListener(this);
@@ -39,12 +53,16 @@ public class Life implements MouseListener, ActionListener, Runnable {
 		fast.addActionListener(this);
 		south.add(slow);
 		slow.addActionListener(this);
+
+		// adds the container south to the frame
 		frame.add(south, BorderLayout.SOUTH);
 
+		// makes frame visible and be able to close
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	// main method that calls the constructor
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Life();
@@ -62,6 +80,8 @@ public class Life implements MouseListener, ActionListener, Runnable {
 	public void mousePressed(MouseEvent event) {
 	}
 
+	// does operation when the mouse is released on a panel cell, switches the cell
+	// from on to off or off to on
 	public void mouseReleased(MouseEvent event) {
 		// TODO Auto-generated method stub
 		double width = (double) panel.getWidth() / cells[0].length;
@@ -74,31 +94,34 @@ public class Life implements MouseListener, ActionListener, Runnable {
 	}
 
 	@Override
+	// runs code for when a button is clicked
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		// if step is clicked, runs the method step();
 		if (event.getSource().equals(step)) {
 			step();
-		} else if (event.getSource().equals(start)) {
+		} else if (event.getSource().equals(start)) {// if start is clicked runs a thread that has the method step();
+														// keep running
 			if (running == false) {
 				running = true;
 				Thread t = new Thread(this);
 				t.start();
 			}
 
-		} else if (event.getSource().equals(stop)) {
+		} else if (event.getSource().equals(stop)) {// if stop is clicked then stops running the method step();
 			running = false;
-		} else if (event.getSource().equals(slow)) {
-			if(speed < 11) {
-				speed ++;
+		} else if (event.getSource().equals(slow)) {// makes the thread run slower if button slow is clicked
+			if (speed < 11) {
+				speed++;
 			}
-		} else if (event.getSource().equals(fast)) {
-			if(speed > 0) {
-				speed --;
+		} else if (event.getSource().equals(fast)) {// makes the thread run faster if button fast is clicked
+			if (speed > 0) {
+				speed--;
 			}
 		}
 	}
 
 	@Override
+	// runs the thread that repeatedly runs the method step();
 	public void run() {
 		// TODO Auto-generated method stub
 		while (running == true) {
@@ -112,7 +135,9 @@ public class Life implements MouseListener, ActionListener, Runnable {
 		}
 	}
 
+	// method that runs 1 generation of the program
 	public void step() {
+		// creates an identical array that represent the next generation
 		boolean[][] nextCells = new boolean[cells.length][cells[0].length];
 		for (int row1 = 0; row1 < cells.length; row1++) {
 			for (int col1 = 0; col1 < cells[0].length; col1++) {
@@ -120,6 +145,7 @@ public class Life implements MouseListener, ActionListener, Runnable {
 			}
 		}
 
+		// checks the amount of neighbors each cell has
 		for (int row = 0; row < cells.length; row++) {
 			for (int col = 0; col < cells[0].length; col++) {
 				int n = 0;
@@ -142,12 +168,13 @@ public class Life implements MouseListener, ActionListener, Runnable {
 					n++;
 				}
 				if (row < cells.length - 1 && col < cells[0].length - 1 && cells[row + 1][col + 1] == true) {// down
-																												// right
+					// right
 					n++;
 				}
 				if (row < cells.length - 1 && cells[row + 1][col] == true) {// down
 					n++;
 				}
+				// checks the conditions of each cell to see if it will survive to the next round, if not assigns false, if so assigns true
 				if (cells[row][col] == true) {
 					if (n == 2 || n == 3) {
 						nextCells[row][col] = true;
@@ -167,12 +194,14 @@ public class Life implements MouseListener, ActionListener, Runnable {
 
 		}
 
+		// makes the original array equal to the next generation array
 		for (int row2 = 0; row2 < cells.length; row2++) {
 			for (int col2 = 0; col2 < cells[0].length; col2++) {
 				cells[row2][col2] = nextCells[row2][col2];
 			}
 		}
 
+		// repaints the frame
 		frame.repaint();
 	}
 
